6d30e6e994fb2afc14732ba7a6d6eec1b4838055
Juan Ignacio Fabrega <54339788+juanifabrega@users.noreply.github.com>
2020-06-25 22:35:26 -0300

    no funca

--- 2proyecto.pl
+++ 2proyecto.pl
@@ -1,5 +1,5 @@
-desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
-    guardarTablero(Tablero).
+/*desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
+    guardarTablero(Tablero).*/
     
 guardarTablero([Fila1,Fila2,Fila3,Fila4,Fila5]):-
     guardarFila1(Fila1),
@@ -14,3 +14,6 @@ guardarFila3([E1,E2,E3,E4,E5]):- celda(3,1,E1),celda(3,2,E2),celda(3,3,E3),celda
 guardarFila4([E1,E2,E3,E4,E5]):- celda(4,1,E1),celda(4,2,E2),celda(4,3,E3),celda(4,4,E4),celda(4,5,E5).
 guardarFila5([E1,E2,E3,E4,E5]):- celda(5,1,E1),celda(5,2,E2),celda(5,3,E3),celda(5,4,E4),celda(5,5,E5).
 
+celda(_,_,_).
+
+guardarTablero([[r1, v2, a1, r1, a1],[a2, v1, v1, r2, r2],[a1, r2, a3, v1, a2],[r3, r2, r1, a3, v1],[v3, a1, v2,r2, v1]]).
