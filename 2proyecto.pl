8f141b394bf3b9f0b2d93155c43631472e9c8e56
Juan Ignacio Fabrega <54339788+juanifabrega@users.noreply.github.com>
2020-07-16 03:26:11 -0300

    actualización de los estados de todas las celdas

--- 2proyecto.pl
+++ 2proyecto.pl
@@ -64,6 +64,25 @@ filaALista(Fila,Lista):- celda(Fila,0,M1,_),
                          celda(Fila,3,M4,_),
                          celda(Fila,4,M5,_),
                          Lista = [M1,M2,M3,M4,M5].
+/*------------------------------------------------------------*/
+/* MOSTRAR TABLEROS DE ESTADOS*/
+/* DPS HAY QUE BORRARLOS*/
+mostrarEstados(T):- estadosATablero(T).
+estadosATablero(T):-filaAListaEstados(0,L1),
+                    filaAListaEstados(1,L2),
+                    filaAListaEstados(2,L3),
+                    filaAListaEstados(3,L4),
+                    filaAListaEstados(4,L5),
+                    T = [L1,L2,L3,L4,L5].
+filaAListaEstados(Fila,Lista):-
+    					 celda(Fila,0,_,M1),
+                         celda(Fila,1,_,M2),
+                         celda(Fila,2,_,M3),
+                         celda(Fila,3,_,M4),
+                         celda(Fila,4,_,M5),
+                         Lista = [M1,M2,M3,M4,M5].        
+/*-----------------------------------------------------*/
+    
 
 insertar_ultimo(X,[],[X]).
 insertar_ultimo(X,[Y|L2],[Y|L3]):- insertar_ultimo(X,L2,L3). 
@@ -176,13 +195,91 @@ buscarColapsoColumna(NumDeColumna,3,L,Lrta):-
 
 buscarTodosLosColapsos(Lrta):-
     buscarColapsosDeFilas([],L1),
-    buscarColapsosDeColumnas(L1,Lrta).
-
-
+    buscarColapsosDeColumnas(L1,Lrta),!.
 
 obtenerCentro([_,E2,_],E2).     % Lista de 3 elementos
 obtenerCentro([_,E2,_,_],E2).   % Lista de 4 elementos
 obtenerCentro([_,_,E3,_,_],E3). % Lista de 5 elementos
+
+siguienteEstado(sincambios,borrar).
+siguienteEstado(borrar,agrandar).
+siguienteEstado(agrandar,agrandar).
+
+obtenerFila([E1,_],E1).
+obtenerColumna([_,E2],E2).
+
+
+
+recorrerLista(Lista):-
+    forall(member(ListaColapso,Lista),
+           (forall(member(Elemento,ListaColapso),
+                   (obtenerFila(Elemento,Fila),
+                   obtenerColumna(Elemento,Columna),
+                   celda(Fila,Columna,Valor,Estado),
+                   siguienteEstado(Estado,NuevoEstado),
+                   retract(celda(Fila,Columna,Valor,Estado)),
+                   assert(celda(Fila,Columna,Valor,NuevoEstado)))))).
+
+tieneCentro([E1,E2,E3,E4,E5]):-
+    obtenerFila(E1,F1),
+    obtenerColumna(E1,C1),
+    celda(F1,C1,_,borrar),
+    obtenerFila(E2,F2),
+    obtenerColumna(E2,C2),
+    celda(F2,C2,_,borrar),
+    obtenerFila(E3,F3),
+    obtenerColumna(E3,C3),
+    celda(F3,C3,_,borrar),
+    obtenerFila(E4,F4),
+    obtenerColumna(E4,C4),
+    celda(F4,C4,_,borrar),
+    obtenerFila(E5,F5),
+    obtenerColumna(E5,C5),
+    celda(F5,C5,_,borrar),
+    ponerCentro([E1,E2,E3,E4,E5]);
+    true.
+    
+tieneCentro([E1,E2,E3,E4]):-
+    obtenerFila(E1,F1),
+    obtenerColumna(E1,C1),
+    celda(F1,C1,_,borrar),
+    obtenerFila(E2,F2),
+    obtenerColumna(E2,C2),
+    celda(F2,C2,_,borrar),
+    obtenerFila(E3,F3),
+    obtenerColumna(E3,C3),
+    celda(F3,C3,_,borrar),
+    obtenerFila(E4,F4),
+    obtenerColumna(E4,C4),
+    celda(F4,C4,_,borrar),
+    ponerCentro([E1,E2,E3,E4]);
+    true.
+    
+tieneCentro([E1,E2,E3]):-
+    obtenerFila(E1,F1),
+    obtenerColumna(E1,C1),
+    celda(F1,C1,_,borrar),
+    obtenerFila(E2,F2),
+    obtenerColumna(E2,C2),
+    celda(F2,C2,_,borrar),
+    obtenerFila(E3,F3),
+    obtenerColumna(E3,C3),
+    celda(F3,C3,_,borrar),
+    ponerCentro([E1,E2,E3]);
+    true.
+    
+ponerCentro(L):-
+    obtenerCentro(L,Rta),
+    obtenerFila(Rta,Fila),
+    obtenerColumna(Rta,Columna),
+    celda(Fila,Columna,Valor,Estado),
+    retract(celda(Fila,Columna,Valor,Estado)),
+    assert(celda(Fila,Columna,Valor,agrandar)).
+    
+recorrerListaDeNuevo(L):-
+    forall(member(ListaColapso,L),
+           tieneCentro(ListaColapso)         
+           ).
 
 
 /*
@@ -199,4 +296,4 @@ guardarTablero([[1, 2, 3, 4, 5],[6, 7, 8, 9, 10],[11, 12, 13, 14, 15],[16, 17, 1
 mostrarTablero(Tablero1),
 mover(der,0,1),
 mostrarTablero(Tablero2).
-*/  
+*/
